ea478ac2be5d241d516c1e6359cd689056544b05
sunil paudel <paudelsgroup@gmail.com>
2025-11-05 06:08:47 +0000

    that is fixed now i am seeing this in the console Fetch finished loading

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,23 +8,23 @@ import { getFirestore } from 'firebase/firestore'
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
     let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
+    
+    // The FIREBASE_CONFIG env var is set automatically by App Hosting.
+    // If it's not set, we're not in an App Hosting environment, so we should
+    // use the config object from the code.
+    if (!process.env.FIREBASE_CONFIG) {
+        firebaseApp = initializeApp(firebaseConfig);
+    } else {
+        // If it is set, we can try to initialize with no arguments.
+        try {
+            firebaseApp = initializeApp();
+        } catch (e) {
+            console.warn('Automatic initialization failed, falling back to firebase config object.', e);
+            firebaseApp = initializeApp(firebaseConfig);
+        }
     }
-
+    
     return getSdks(firebaseApp);
   }
 
