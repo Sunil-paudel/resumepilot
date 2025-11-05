65d982bc2d5e63e844e495118a7a70c2d7128102
sunil paudel <paudelsgroup@gmail.com>
2025-11-05 03:42:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6388470099-7623b",
+  "appId": "1:5813629755:web:51894209c8775ec4087d1e",
+  "apiKey": "AIzaSyAzMfy7JyMgJKeWEjF3wTNwS1RbRXn7kiE",
+  "authDomain": "studio-6388470099-7623b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "5813629755"
+};
